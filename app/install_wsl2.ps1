29d10455ef0cffde64d56ed2ed5a816e3875c186
Mårten Cassel <marten.cassel@proact.se>
2024-10-25 16:51:36 +0200

    more

--- app/install_wsl2.ps1
+++ app/install_wsl2.ps1
@@ -1,9 +1,8 @@
 try {
     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-    wsl --install
-    wsl --set-default-version 2
     wsl --update
+    wsl --set-default-version 2
     exit 1641
 } catch {
     Write-Error "Failed to install WSL2"
