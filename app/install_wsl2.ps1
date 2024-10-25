5fd09ac7dfd40c6cc2d48381a075117d1ef23dcf
Mårten Cassel <marten.cassel@proact.se>
2024-10-25 17:21:48 +0200

    more

--- app/install_wsl2.ps1
+++ app/install_wsl2.ps1
@@ -1,8 +1,6 @@
 try {
     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-    wsl --update
-    wsl --set-default-version 2
     exit 1641
 } catch {
     Write-Error "Failed to install WSL2"
--- app/uninstall_wsl2.ps1
+++ app/uninstall_wsl2.ps1
@@ -1,7 +1,6 @@
 try {
     dism.exe /online /disable-feature /featurename:Microsoft-Windows-Subsystem-Linux /norestart
     dism.exe /online /disable-feature /featurename:VirtualMachinePlatform /norestart
-    wsl --uninstall
     exit 1641
 } catch {
     Write-Error "Failed to uninstall WSL2"
