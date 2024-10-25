4f55df6940c6c593188b8febddcec217569d6a90
Mårten Cassel <marten.cassel@proact.se>
2024-10-25 16:34:07 +0200

    more

--- app/detect_wsl2_install.ps1
+++ app/detect_wsl2_install.ps1
@@ -1,13 +1,12 @@
 $wslFeature = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 $vmPlatformFeature = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 
-$vmPlatformIsEnabled = $($vmPlatformFeature.State -eq "EnablePending") -or ($vmPlatformFeature.State -eq "Enabled")
-$wslFeatureIsEnabled = $wslFeature.State -eq "Enabled"
 
-if ($wslFeatureIsEnabled -and $vmPlatformIsEnabled) {
-    Write-Output "Windows Subsystem for Linux is installed and configured correctly with Virtual Machine Platform."
+# How do we check if these features are enabled?
+if ($wslFeature.State -eq "Enabled" -and $vmPlatformFeature.State -eq "Enabled") {
+    Write-Output "Windows Subsystem for Linux is installed and configured with Virtual Machine Platform."
     exit 0
 } else {
-    Write-Output "Windows Subsystem for Linux is not installed or configured correctly with Virtual Machine Platform."
+    Write-Output "Windows Subsystem for Linux and Virtual Machine Platform are not installed."
     exit 1
 }
