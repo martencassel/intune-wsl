96e7df85b78d59d919e7b6cba9a2e4493dadcf41
Mårten Cassel <marten.cassel@proact.se>
2024-10-26 15:25:12 +0200

    More

--- app/detect_wsl2_install.ps1
+++ app/detect_wsl2_install.ps1
@@ -1,10 +1,24 @@
 $wslFeature = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-$vmPlatformFeature = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+$wslFeatureEnabled = $wslFeature | Select-Object -ExpandProperty State
 
-if ($wslFeature.State -eq "Enabled" -and $vmPlatformFeature.State -eq "Enabled") {
+$vmPlatformFeature = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+$vmPlatformFeatureEnabled = $vmPlatformFeature | Select-Object -ExpandProperty State
+
+
+if ( ($wslFeatureEnabled -eq "Enabled") -and ($vmPlatformFeatureEnabled -eq "Enabled") ) {
     Write-Output "WSL2 is installed and configured correctly."
     exit 0
-} else {
-    Write-Output "WSL2 is not installed or not configured correctly."
-    exit 1
+} 
+
+$ErrMsg = ""
+
+if ($wslFeatureEnabled -ne "Enabled") {
+    $ErrMsg = "wslFeatureEnabled is not enabled"
 }
+
+if ($vmPlatformFeatureEnabled -ne "Enabled") {
+    $ErrMsg = $ErrMsg + " vmPlatformFeatureEnabled is not enabled"
+}
+
+Write-Output $ErrMsg
+exit 1
