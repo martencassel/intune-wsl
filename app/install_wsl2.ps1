faa8098fc6fb876d402bc000c1948600bcc91c7f
marten <marten.cassel@gmail.com>
2024-10-28 15:33:03 +0100

    Update install_wsl2.ps1

--- app/install_wsl2.ps1
+++ app/install_wsl2.ps1
@@ -1,32 +1,21 @@
-
 try {
     $LogPath = "C:\temp\wsl2_install.log"
     Start-Transcript -Path $LogPath -Append
-
-    # Check for pending restart
-    $pendingRestart = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -ErrorAction SilentlyContinue)
-    if ($pendingRestart) {
-        Write-Output "A pending restart is detected. Please restart the system and run the script again."
-        Write-Output "Returning exit code 1 to indicate a pending restart."
-        exit 1
-    }
-
+    Write-Host "Installing Microsoft-Windows-Subsystem-Linux to enable WS1 support"
     $wslFeature = Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
     if ($wslFeature.State -eq "Enabled") {
-        Write-Output "Windows Subsystem for Linux is already installed."
+        Write-Host "Windows Subsystem for Linux is already installed."
     } else {
         Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
-        Write-Output "Windows Subsystem for Linux installed successfully."
+        Write-Host "Windows Subsystem for Linux installed successfully."
     }
-
     $vmPlatformFeature = Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
     if ($vmPlatformFeature.State -eq "Enabled") {
-        Write-Output "Virtual Machine Platform is already installed."
+        Write-Host "Virtual Machine Platform is already installed."
     } else {
         Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -NoRestart
-        Write-Output "Virtual Machine Platform installed successfully."
+        Write-Host "Virtual Machine Platform installed successfully."
     }
-
     Write-Output "Returning exit code 1641 to trigger a restart by Intune."
     exit 1641
 } catch {
