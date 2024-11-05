ca631e7bbbbb48b6c221299b71418c335198b17f
unknown <marten.cassel@gmail.com>
2024-11-05 08:36:32 +0100

    more

--- build-wsl2-app.bat
+++ build-wsl2-app.bat
@@ -1,2 +1,2 @@
-dir .\build\fail-app\install.intunewin
+copy C:\users\testuser\source\repos\install-wsl2\Release\install-wsl2.exe .\wsl2-app\
 .\tools\IntuneWinAppUtil -c .\wsl2-app -s .\wsl2-app\install.ps1  -o .\build\wsl2-app\
